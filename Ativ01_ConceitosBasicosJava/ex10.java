8708f992b88936519b2f11b6a3c259de26be503f
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-08-15 14:52:33 -0300

    fazendo mais exercicios

--- Ativ01_ConceitosBasicosJava/ex10.java
+++ Ativ01_ConceitosBasicosJava/ex10.java
@@ -1,1 +1,8 @@
-// Faça um programa na Linguagem Javapara efetuar a leitura de um número inteiro e apresentar o resultado do quadrado desse número\
+// Faça um programa na Linguagem Javapara efetuar a leitura de um número inteiro e apresentar o resultado do quadrado desse número
+public class ex10{ 
+	public static void main(String[] args) {
+        int num = 9;
+
+        System.out.println("O quadrado do número " + num + ": " + num * num);
+	}
+}
