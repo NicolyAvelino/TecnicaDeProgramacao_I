4fdf892ad3d379c7fe21901268c2572e67146d97
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-08-17 17:38:09 -0300

    novo

---
+++ Ativ02_Condicional&Loopings/ex31.java
@@ -0,0 +1,23 @@
+// Faça um programa na Linguagem Java que que receba um número e mostre o fatorial desse número.
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		// declaração de variável
+		int n = 6;
+        int ft = n;
+
+		// cria um objeto scanner
+		Scanner sc = new Scanner(System.in);
+ 
+		// receber valor do usuario
+        // System.out.print("Digite um número: ");
+		// n = sc.nextInt();
+
+		// realizando operação
+        while(n > 1){
+            ft = ft * (n -1);
+        }
+        // saida de dados
+        System.out.printf("O fatorial do número %d é: %d.",n,ft);
+	}
+}
