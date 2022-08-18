5742dc1f6647b56d368911cfc6ee64709df2add4
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-08-18 12:13:49 -0300

    salvar

--- Ativ01_ConceitosBasicosJava/ex05-idade.java
+++ Ativ01_ConceitosBasicosJava/ex05-idade.java
@@ -1,29 +1,32 @@
 // Faça  um programa na Linguagem Java que leia  a  idade  de  uma pessoa expressa em ano, mês e dia e mostre-as em dias.
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 public class Main{
     public static void main(String[] args){
         // declaração de variáveis
-        int ano, mes, dia, qtDias;
+        String data; 
 
         // cria um objeto scanner
         Scanner sc = new Scanner(System.in);
 
         // Entrada de dados
-        System.out.print("Digite o dia em que nasceu: ");
-        dia = sc.nextInt();
+        System.out.print("Digite a data do seu nascimento (dia/mes/ano): ");
+        data = sc.nextLine();
 
-        // Entrada de dados
-        System.out.print("Digite o mês em que nasceu: ");
-        mes = sc.nextInt();
+        // transforma em array
+        String[] dtSep = data.split("/"); 
 
-        // Entrada de dados
-        System.out.print("Digite o ano em que nasceu: ");
-        ano = sc.nextInt();
+        // passa os valores para formato de data
+        LocalDate dtIn = LocalDate.of(Integer.parseInt(dtSep[2]), Integer.parseInt(dtSep[1]), Integer.parseInt(dtSep[0]));
 
-        // cálculo da formúla  
-        qtDias = (ano * 365) + (mes * 30) + dia;
+        // pega a data de hoje
+        LocalDate dtHoje = LocalDate.now();
+
+        // cria variavel de dias tipo long e calcula a diferenca entre as datas
+        long qtDias = ChronoUnit.DAYS.between(dtIn,dtHoje);
 
         // saida de dados
-        System.out.printf("A quantidades tempo você já viveu em dias: %d.", qtDias);
-	}
+        System.out.printf("A quantidade de tempo você já viveu em dias: %d.", qtDias);
+    }
 }
