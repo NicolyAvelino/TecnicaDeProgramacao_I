c35c836fb54f1394910b752a28795c29fecda107
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-08-24 15:56:22 -0300

    up

--- Ativ02_Condicional&Loopings/ex16-ordem.java
+++ Ativ02_Condicional&Loopings/ex16-ordem.java
@@ -53,31 +53,25 @@ public class Main {
         n3 = sc.nextInt();
 
         // realizando operação usando if else
-		if(n3 > n1){
-			if(n1 > n2){
-				// saida de dados
-				System.out.printf("Ordem crescente dos números: %d, %d, %d.",n2,n1,n3);
-			}else if(n2 < n3){
-				// saida de dados
-				System.out.printf("Ordem crescente dos números: %d, %d, %d.",n1,n2,n3);
-			} 
-			if(n1 > n2) {
-				if(n2 > n3){
-					// saida de dados
-					System.out.printf("Ordem crescente dos números: %d, %d, %d.",n3,n2,n1);
-				} else if(n3 < n1){
-					// saida de dados
-					System.out.printf("Ordem crescente dos números: %d, %d, %d.",n2,n3,n1);
-				} 
+		if (n3 > n1){
+			if (n1 > n2){
+			System.out.printf("Ordem crescente dos números: %d, %d, %d.", n2, n1, n3);
+			} else if (n2 < n3){
+				System.out.printf("Ordem crescente dos números: %d, %d, %d.", n1, n2, n3);
 			}
-			if(n1 < n2){
-				if(n3 < n1){
-					// saida de dados
-					System.out.printf("Ordem crescente dos números: %d, %d, %d.",n3,n1,n2);
-				} else if(n3 < n2){
-					// saida de dados
-					System.out.printf("Ordem crescente dos números: %d, %d, %d.",n1,n3,n2);
-				}
+		}
+		if (n1 > n2){
+			if (n2 > n3){
+			System.out.printf("Ordem crescente dos números: %d, %d, %d.", n3, n2, n1);
+			} else if (n3 < n1){
+				System.out.printf("Ordem crescente dos números: %d, %d, %d.", n2, n3, n1);
+			}
+		}
+		if (n1 < n2){
+			if (n3 < n1){
+			System.out.printf("Ordem crescente dos números: %d, %d, %d.", n3, n1, n2);
+			} else if (n3 < n2){
+				System.out.printf("Ordem crescente dos números: %d, %d, %d.", n1, n3, n2);
 			}
 		}
 	}
