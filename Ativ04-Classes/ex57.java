998e50d585261de5dce65b393ba28f18cf1e29f2
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-08-26 14:37:31 -0300

    up

--- Ativ04-Classes/ex57.java
+++ Ativ04-Classes/ex57.java
@@ -4,3 +4,11 @@
 // • CPF;
 // • Saldo Da conta.
 // Em seguida, mostrar a tela todos os atributos. Utilize classe
+public class Main{
+    public static void main[String[] args]{
+        private int nr_Conta;
+        private String nm_Cliente;
+        private int nr_CPF;
+        private double saldo;
+    }
+}
--- Ativ04-Classes/ex58.java
+++ Ativ04-Classes/ex58.java
@@ -1,1 +1,9 @@
 // Objetivo: No exercício anterior crie 2 métodos
+public class Main{
+    public static void main[String[] args]{
+        private int nr_Conta;
+        private String nm_Cliente;
+        private int nr_CPF;
+        private double saldo;
+    }
+}
