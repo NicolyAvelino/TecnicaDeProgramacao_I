010a95a45e871f956286a1e002ec7346f07c8fc4
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-08-27 14:46:02 -0300

    up

--- Ativ04-Classes/ex57.java
+++ Ativ04-Classes/ex57.java
@@ -4,11 +4,25 @@
 // • CPF;
 // • Saldo Da conta.
 // Em seguida, mostrar a tela todos os atributos. Utilize classe
+public class ContaCorrente{
+    // criação da classe
+    // declarando dos atributos
+    private int nr_Conta;
+    private String nm_Cliente;
+    private int nr_CPF;
+    private double saldo;
+}
+
+// Instanciar
 public class Main{
-    public static void main[String[] args]{
-        private int nr_Conta;
-        private String nm_Cliente;
-        private int nr_CPF;
-        private double saldo;
+    public static void main(Strin[] args){
+        ContaCorrente cc = new ContaCorrente();
+
+        cc.nr_Conta = 1234;
+        cc.nm_Cliente = "Maria da Silva";
+        cc.nr_CPF = 87654323499;
+        cc.saldo = 2000;
+
+        System.out.printf("Número da Conta: %f\nNome Cliente: %s\nNúmero CPF: %d\n Saldo: %f",cc.nr_Conta,cc.nm_Cliente,cc.nr_CPF,cc.saldo);
     }
 }
--- Ativ04-Classes/ex58.java
+++ Ativ04-Classes/ex58.java
@@ -1,9 +1,45 @@
 // Objetivo: No exercício anterior crie 2 métodos
 public class Main{
-    public static void main[String[] args]{
-        private int nr_Conta;
-        private String nm_Cliente;
-        private int nr_CPF;
-        private double saldo;
+    // criação da classe
+    // declarando dos atributos
+    private int nr_Conta;
+    private String nm_Cliente;
+    private int nr_CPF;
+    private double saldo;
+
+    // criação do metodo depositar
+    public void valor(double valorDepositar){
+        saldo += valorDepositar;
+        System.out.printf("Seu saldo atual é R$%.2f",saldo);
+    }
+    // criação do metodo saque do tipo boolean
+    boolean saque(double qtSacar){
+        // tem saldo na conta
+        if(saldo >= qtSacar){
+            saldo -= qtSacar;
+            return true;
+            System.out.print("Saque efetuado!");
+        } else{ 
+            return false 
+            System.out.print("Não foi possível realizar o saque!");
+        }
+    }
+}
+
+
+// Instanciar
+public class Main{
+    public static void main(Strin[] args){
+        ContaCorrente cc = new ContaCorrente();
+
+        cc.nr_Conta = 1234;
+        cc.nm_Cliente = "Maria da Silva";
+        cc.nr_CPF = 87654323499;
+        cc.saldo = 2000;
+        cc.saque(20);
+        cc.valor(200);
+
+        System.out.printf("Número da Conta: %f\nNome Cliente: %s\nNúmero CPF: %d\n Saldo: %f",cc.nr_Conta,cc.nm_Cliente,cc.nr_CPF,cc.saldo);
+       
     }
 }
