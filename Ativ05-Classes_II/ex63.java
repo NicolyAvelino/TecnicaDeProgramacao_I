06852c188a938e2abb0e1cbc742660bbb0536a45
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-09-01 16:23:10 -0300

    up

--- Ativ05-Classes_II/ex63.java
+++ Ativ05-Classes_II/ex63.java
@@ -11,19 +11,34 @@
 // Diagonal d = raiz a² + b²
 public class Retangulo{
     // declarando os atributos
-    double largura;
-    double altura;
+    double largura=3.00;
+    double altura=4.00;
 
     // criando o método area()
     public void area(){
-
+        return largura * altura;
     }
     // criando o método perimetro()
     public void perimetro(){
-        
+        return 2 * (largura + altura);
     }
     // criando o método diagonal()
     public void diagonal(){
+        return Math.sqrt(Math.pow(altura,2) + Math.pow(largura,2));
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        Retangulo rt = new Retangulo();
+        // entrada de dados
         
+        rt.area();
+        rt.perimetro();
+        rt.diagonal();
+
+        // saida de dados
+        System.out.print("AREA = %.2f \nPERIMETRO = %.2f \nDIAGONAL = %.2f",rt.area(),rt.perimetro(),rt.diagonal());
+
     }
 }
