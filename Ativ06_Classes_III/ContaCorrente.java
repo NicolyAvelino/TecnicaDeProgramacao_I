47c5aa21a1cfa8c38fba80dc1df91c8084841d52
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-09-14 10:03:15 -0300

    ativ06

---
+++ Ativ06_Classes_III/ContaCorrente.java
@@ -0,0 +1,15 @@
+// 67. Criar uma classe para representar uma entidade que representa uma
+// Conta Corrente em um sistema bancário. A conta corrente deve ter os
+// seguintes atributos (todos privados):
+// • Nome do Correntista
+// • Sexo
+// • Idade
+// • Numero da Conta
+// • Flag indicando se é conta conjunta
+// • Saldo
+public class ContaCorrente{
+    // declarando atributos
+    private String nome, nr_Conta, sexo;
+    private int idade;
+    private double saldo;
+}
