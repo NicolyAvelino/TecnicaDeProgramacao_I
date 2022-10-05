7f9942efccff13bec50d7def126dcd6d4a48771c
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-10-05 15:24:34 -0300

    heranca 02

--- Ativ09_Heranca/heranca_02.java
+++ Ativ09_Heranca/heranca_02.java
@@ -14,4 +14,60 @@ public class Veiculo{
     public void setPlaca(String placa){
         this.placa = placa;
     }
+
+    public void setAno(int ano){
+        this.ano = ano;
+    }
+    public void setAno(String anoString){
+        this.ano = anoString;
+    }
+
+    public String getPlaca(){
+        return placa;
+    }
+    public int getAno(){
+        return ano;
+    }
+    public void exibirDados(){
+
+    }
+
+}
+
+public class Onibus extends Veiculo{
+    private int assentos;
+
+    public Onibus(String placa, int ano, int assentos){
+        this.placa = placa;
+        this.ano = ano;
+        this.assentos = assentos;
+    }
+    public void setAssentos(int assentos){
+        this.assentos = assentos;
+    }
+    public int getAssentos(){
+        return assentos;
+    }
+    public void exibirDados(){
+
+    }
+}
+
+public class Caminhao extends Veiculo{
+    private int eixos;
+
+    public Caminhao(String placa, int ano, int eixos){
+        this.placa = placa;
+        this.ano = ano;
+        this.eixos = eixos;
+    }
+    public void setEixos(int eixos){
+        this.eixos = eixos;
+    }
+    public int getEixos(){
+        return eixos;
+    }
+    public void exibirDados(){
+        
+    }
 }
